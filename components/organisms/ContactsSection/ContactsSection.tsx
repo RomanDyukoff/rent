3ea4e703e03c9b9e31ed9a8a13805618f61e5621
DESKTOP-4M5140Q\romad <hlebahleb@gmail.com>
2024-01-12 23:41:57 +0300

    feat: added contact sections

--- components/organisms/ContactsSection/ContactsSection.tsx
+++ components/organisms/ContactsSection/ContactsSection.tsx
@@ -1,7 +1,46 @@
-import styles from './style.module.scss'
+'use client';
 
-export const ContactsSection = () => {
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import { Map, YMaps } from '@pbe/react-yandex-maps';
+
+import { Title } from '@/components/atoms/Title/Title';
+
+import { Container } from '../../atoms/Container/Container';
+
+import styles from './style.module.scss';
+
+export const ContactsSection = (): JSX.Element => {
 	return (
-		<section id='contacts' className={styles.contacts}>ContactsSection</section>
-	)
-}
+		<section className={`${styles.contacts} section-container`} id="contacts">
+			<Container>
+				<Title levet="h2" classNames="section-title title">
+					Контакты
+				</Title>
+				<div className={styles.contacts__content}>
+					<div className={styles.info}>
+						<Title levet="h5" classNames={styles.info__title}>
+							По всем вопросам обращайтесь:
+						</Title>
+						<address className={styles.info__address}>
+							<a href="#">Для получения информации: (617) 730-2370</a>
+							<a href="#">Сделать замеры (617) 730-2370</a>
+							<a href="#">Обсудить цену: (617) 730-2385</a>
+							<span>Наш адресс: г. Витебск ул. Фрунзе</span>
+						</address>
+					</div>
+					<div className={styles.contacts__map}>
+						<YMaps>
+							<Map
+								width="100%"
+								height="400px"
+								defaultState={{ center: [55.184218, 30.202877], zoom: 9 }}
+							/>
+						</YMaps>
+					</div>
+				</div>
+			</Container>
+		</section>
+	);
+};
+
