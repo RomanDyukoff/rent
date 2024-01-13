f7e52a161e1f0970f15d91d3738927d7c8cf200e
DESKTOP-4M5140Q\romad <hlebahleb@gmail.com>
2024-01-13 13:27:52 +0300

    fix: welcome

--- components/organisms/RequirementSection/RequirementSection.tsx
+++ components/organisms/RequirementSection/RequirementSection.tsx
@@ -1,7 +1,12 @@
+import { Container } from '@/components/atoms/Container/Container'
 import styles from './style.module.scss'
 
 export const RequirementSection = () => {
 	return (
-		<section id='requi' className={styles.requirement}>Requirement</section>
+		<section id='requi' className={styles.requirement}>
+			<Container>
+				Requirement
+			</Container>
+		</section>
 	)
 }
