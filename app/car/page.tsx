85100454f6193a039678fa8b623e2874e7e58ea4
DESKTOP-4M5140Q\romad <hlebahleb@gmail.com>
2024-01-14 04:26:41 +0300

    start: create page carInfo

--- app/car/page.tsx
+++ app/car/page.tsx
@@ -1,7 +1,13 @@
+import { Container } from '@/components/atoms/Container/Container';
+import styles from './style.module.scss'
+
+
 export default function CarInfo(): JSX.Element {
     return (
-        <div>
-            car
-        </div>
+        <section className={styles.car}>
+            <Container>
+                <h1 className={styles.car__title}>Описание авто</h1>
+            </Container>
+        </section>
     );
 };
